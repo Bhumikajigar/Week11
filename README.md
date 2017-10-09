9a07b5ceb55243e30b8dac3d5295c32b55aaf322
Bhumikajigar <30588755+Bhumikajigar@users.noreply.github.com>
2017-10-09 14:45:01 +1100

    Add all files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Week11
+
+a [Sails](http://sailsjs.org) application
---
+++ api/models/User.js
@@ -0,0 +1,42 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+ 
+	title:
+	{
+	type: 'string',
+	required: true
+	},
+
+	releasedate: {
+	type:'Date',
+	required: true
+	},
+
+	Duration:
+	{
+		type: 'string',
+		required: true
+	},
+
+	Genre:
+	{
+		type: 'string',
+		required: true
+	},
+	Synopsis:
+	{
+		type: 'string',
+		required: true
+	}
+
+	}
+};
+
